fd5e2acef30f0e5e986c8343b9d0dda1386abde3
Chanda <chandapiwamalema5@gmail.com>
2025-12-19 10:24:16 +0200

    cache submissions

--- frontend/src/public/sw.js
+++ frontend/src/public/sw.js
@@ -2,7 +2,7 @@ import { clientsClaim } from 'workbox-core';
 import {precacheAndRoute} from 'workbox-precaching/precacheAndRoute';
 import { registerRoute } from 'workbox-routing/registerRoute';
 import { BackgroundSyncPlugin } from 'workbox-background-sync/';
-import {NetworkOnly, StaleWhileRevalidate} from 'workbox-strategies';
+import {NetworkOnly, NetworkFirst, StaleWhileRevalidate} from 'workbox-strategies';
 
 const bgSyncPlugin = new BackgroundSyncPlugin('api-queue', {
   maxRetentionTime: 24 * 60, // Retry for max of 24 Hours
@@ -16,6 +16,11 @@ precacheAndRoute(self.__WB_MANIFEST);
 registerRoute(
     ({ request }) => request.method === 'POST',
     new NetworkOnly({ plugins: [bgSyncPlugin] }), 'POST'
+);
+
+registerRoute(
+  ({ url }) => url.pathname.startsWith('/api/submissions'),
+  new NetworkFirst({ cacheName: 'api-cache' })
 );
 
 registerRoute(
