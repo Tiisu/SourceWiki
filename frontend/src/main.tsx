a6d49d34154aa172b1301376e76c3140c8b99806
Abanga39 <aminabdulmuhsin99@gmail.com>
2025-12-20 18:42:22 +0000

    Fixed missing Router in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@
 
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
+  import { BrowserRouter } from 'react-router-dom';
   import "./index.css";
   import { Workbox } from "workbox-window";
 
